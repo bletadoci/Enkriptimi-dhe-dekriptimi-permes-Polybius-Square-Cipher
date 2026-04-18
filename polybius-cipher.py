b49764c1301f7284a09686275e464835658fabb1
blertalatifi <blerta.latifi4@student.uni-pr.edu>
2026-04-18 15:49:09 +0200

    Implement create_polybius_square function to generate Polybius square mapping

---
+++ polybius-cipher.py
@@ -0,0 +1,8 @@
+def create_polybius_square():
+    square = {}
+    al0phabet = 'ABCDEFGHIKLMNOPQRSTUVWXYZ'  
+    for index, letter in enumerate(alphabet):
+        row = (index // 5) + 1
+        col = (index % 5) + 1
+        square[letter] = (row, col)
+    return square
